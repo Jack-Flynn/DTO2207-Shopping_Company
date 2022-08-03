76425c669b4c701363f8080091737b99e28b3bef
Jack-Flynn <flynn.jack@awatapu.school.nz>
2022-08-04 10:44:22 +1200

    Box volume work in progress

---
+++ box_volume.py
@@ -0,0 +1,27 @@
+# Number checker
+def int_check(question, low_num, high_num):
+    error = "Your specified box is outside of our capabilities, please enter a whole number between {} and {}".format(low_num, high_num)
+
+    valid = False
+    while not valid:
+        try:
+            response = int(input(question))
+            if low_num <= response <= high_num:
+                return response
+            else:
+                print(error)
+        except ValueError:
+            print(error)
+
+
+def volume_math (num1, num2, num3):
+    num1 * num2 * num3
+
+
+# Ask for the dimensions
+height = int_check("Please enter the required height", 5, 100)
+width = int_check("Please enter the required width", 5, 100)
+depth = int_check("Please enter the required depth.", 5, 100)
+
+volume = volume_math(height, width, depth)
+print(volume)
