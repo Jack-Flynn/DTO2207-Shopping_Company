3769a0506081390ca1eacdfacb3d6b4164001553
Jack-Flynn <flynn.jack@awatapu.school.nz>
2022-08-05 11:54:55 +1200

    Extra comment + grammar changes on box volume also a new file

--- box_volume.py
+++ box_volume.py
@@ -1,6 +1,6 @@
 # Number checker
 def int_check(question, low_num, high_num):
-    error = "Your specified box is outside of our capabilities, please enter a whole number between {} and {}".format(low_num, high_num)
+    error = "Your specified box is outside of our capabilities, please enter a whole number between {} and {}.".format(low_num, high_num)
 
     valid = False
     while not valid:
@@ -14,15 +14,16 @@ def int_check(question, low_num, high_num):
             print(error)
 
 
+# Finding the volume
 def volume_math (num1, num2, num3):
     volume = num1 * num2 * num3
-    print("the volume is {}".format(volume))
+    print("The volume on this box is {}.".format(volume))
 
 
 # Ask for the dimensions
-height = int_check("Please enter the required height", 5, 100)
-width = int_check("Please enter the required width", 5, 100)
-depth = int_check("Please enter the required depth", 5, 100)
+height = int_check("Please enter the required height.", 5, 100)
+width = int_check("Please enter the required width.", 5, 100)
+depth = int_check("Please enter the required depth.", 5, 100)
 
 volume_math(height, width, depth)
 
